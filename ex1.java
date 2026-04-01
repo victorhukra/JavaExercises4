72ce670b6db5468a0450949c6e417ebb5c95a7fa
victormaximiano <128063057+victorhukra@users.noreply.github.com>
2026-04-01 20:59:22 -0300

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,17 @@
+
+
+
+//1-) Desenvolva um algoritmo que imprima os valores de 1 até 100 (Utilizar a ;
+//estrutura FOR). 
+
+package com.mycompany.lista3;
+
+public class ex1 {
+    public static void main(String[] args) {
+        
+        for (int i=1; i<101; i++){
+            System.out.println(i);
+
+        }
+    }
+}
