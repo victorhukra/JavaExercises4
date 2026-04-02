aba625f793ebab69fcb5bf8d37b9182f0faea0f0
victormaximiano <128063057+victorhukra@users.noreply.github.com>
2026-04-01 21:07:12 -0300

    Move ex1 class to a new package 'com.mycompany.lista4'

--- ex1.java
+++ ex1.java
@@ -4,7 +4,7 @@
 //1-) Desenvolva um algoritmo que imprima os valores de 1 até 100 (Utilizar a ;
 //estrutura FOR). 
 
-package com.mycompany.lista3;
+package com.mycompany.lista4;
 
 public class ex1 {
     public static void main(String[] args) {
