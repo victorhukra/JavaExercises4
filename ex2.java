f3833ce645d2af7876d1f1288e8c02a2908f06a8
victormaximiano <128063057+victorhukra@users.noreply.github.com>
2026-04-01 21:06:35 -0300

    Add files via upload

---
+++ ex2.java
@@ -0,0 +1,15 @@
+//2-) Desenvolva um algoritmo que imprima na tela, somente os números 
+//múltiplos de 5, de 1 até 1000 (Utilizar a estrutura FOR).
+
+package com.mycompany.lista3;
+
+public class ex2 {
+    public static void main(String[] args) {
+        
+    for (int i = 5; i <= 1000; i += 5) {
+        System.out.println(i);
+        
+    }
+  }
+}
+
